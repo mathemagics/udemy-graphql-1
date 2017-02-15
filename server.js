b654b26d82b66735da7544ad7111515b1f4555d2
Paul WB <higgsbison@gmail.com>
2017-02-15 13:43:14 -0800

    boilerplate express and graphql express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql')
+const app = express();
+
+app.listen(4000, () => {
+  console.log('listening');
+});
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}))
