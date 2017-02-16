21a84ab70f1008b559ac06184d64afe50a42d0ab
Paul WB <higgsbison@gmail.com>
2017-02-15 16:37:20 -0800

    associate company with user

--- schema/schema.js
+++ schema/schema.js
@@ -25,6 +25,10 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(res => res.data);
+      }
     }
   }
 });
